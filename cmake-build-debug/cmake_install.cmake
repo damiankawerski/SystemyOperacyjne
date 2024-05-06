0777d52da563c477f179f2cea00c4a6c42e58fd3
Damian Kawerski <damian.kawerski@gmail.com>
2024-05-06 16:14:09 +0200

    After koolos 1

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/damian/GitHub/SO
+# Install script for directory: /home/damian/GitHub/SystemyOperacyjne
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/damian/GitHub/SO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/damian/GitHub/SystemyOperacyjne/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -2,7 +2,36 @@
 #include "Lab5.h"
 #include "Lab4.h"
 #include "Lab6.h"
-int main()
-{
-    Lab5();
+#include <sys/types.h>
+#include <wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main() {
+    char *fifoPath = "/tmp/fifo";
+    pid_t pid1, pid2;
+
+    mkfifo(fifoPath, 0666);
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        int fd = open(fifoPath, O_WRONLY);
+        write(fd, "HALLO!", 7);
+        close(fd);
+    } else if (pid1 > 0) {
+        pid2 = fork();
+
+        if (pid2 == 0) {
+            wait(&pid1);
+
+            char str[7];
+
+            int fd = open(fifoPath, O_RDONLY);
+            read(fd, str, 7);
+            printf("%s\n", str);
+            close(fd);
+        }
+    }
 }
